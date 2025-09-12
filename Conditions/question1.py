f731b9c5ae2c1760dc2c9c4a24721afb1089792a
rdevang71 <rdevang71@gmail.com>
2025-09-12 22:25:45 +0530

    Q1

---
+++ Conditions/question1.py
@@ -0,0 +1,9 @@
+age = int(input("Please Enter  age"))
+if(age<13):
+    print("The person is a child")
+elif(19>=age>=13):
+    print("person is Teenager")
+elif(59>=age>=20):
+    print("person is Teenager")
+elif(60<=age):
+    print("person is Senior")
